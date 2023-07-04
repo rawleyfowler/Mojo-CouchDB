b4ed50e0b792bf0cb2f9c7e5cea22fd357e5ccd4
Rawley Fowler <rawleyfowler@proton.me>
2023-07-04 11:13:59 -0600

    fixup pod

--- lib/Mojo/CouchDB.pm
+++ lib/Mojo/CouchDB.pm
@@ -6,16 +6,29 @@ use Mojo::UserAgent;
 use Mojo::IOLoop;
 
 use Carp qw(croak carp);
+use URI;
 use MIME::Base64;
 use Scalar::Util qw(reftype);
-
-use feature 'say';
 
 our $VERSION = '0.1';
 
 has 'url';
 has 'auth';
 has ua => sub { Mojo::UserAgent->new };
+
+sub all_docs {
+    my $self  = shift;
+    my $query = $self->_to_query(shift);
+    return $self->_call('_all_docs' . $query, 'get')->result->json;
+}
+
+sub all_docs_p {
+    my $self  = shift;
+    my $query = $self->_to_query(shift);
+
+    return $self->_call('_all_docs' . $query, 'get_p')
+        ->then(sub { return shift->res->json });
+}
 
 sub create_db {
     my $status = shift->_call('', 'put')->result->code;
@@ -27,6 +40,33 @@ sub create_db {
     return undef;
 }
 
+sub find {
+    my $self = shift;
+    my $sc   = shift;
+    return $self->_find($sc)->_call('_find', 'post', $sc)->result->json;
+}
+
+sub find_p {
+    my $self = shift;
+    my $sc   = shift;
+    return $self->_find($sc)->_call('_find', 'post_p', $sc)
+        ->then(sub { return shift->res->json });
+}
+
+sub index {
+    my $self = shift;
+    my $idx  = shift;
+
+    return $self->_index($idx)->_call('_index', 'post', $idx)->result->json;
+}
+
+sub index_p {
+    my $self = shift;
+    my $idx  = shift;
+
+    return $self->_index($idx)->_call('_index', 'post_p', $idx)
+        ->then(sub { return shift->res->json });
+}
 
 sub new {
     my $self     = shift->SUPER::new;
@@ -81,19 +121,6 @@ sub save_many_p {
         ->then(sub { return shift->res->json });
 }
 
-sub find {
-    my $self = shift;
-    my $sc   = shift;
-    return $self->_find($sc)->_call('_find', 'post', $sc)->result->json;
-}
-
-sub find_p {
-    my $self = shift;
-    my $sc   = shift;
-    return $self->_find($sc)->_call('_find', 'post_p', $sc)
-        ->then(sub { return shift->res->json });
-}
-
 sub _call {
     my $self   = shift;
     my $loc    = shift;
@@ -117,9 +144,19 @@ sub _find {
     my $self = shift;
     my $sc   = shift;
 
-    croak qq{Invalid type supplied for search criteria, expected hashref get: }
-        . (reftype $sc)
+    croak qq{Invalid type supplied for search criteria, expected hashref got: }
+        . reftype $sc
         unless reftype($sc) eq 'HASH';
+
+    return $self;
+}
+
+sub _index {
+    my $self = shift;
+    my $idx  = shift;
+
+    croak qq{Invalid type supplied for index, expected hashref got: } . reftype $idx
+        unless reftype($idx) eq 'HASH';
 
     return $self;
 }
@@ -145,6 +182,18 @@ sub _save_many {
         unless (reftype($docs) eq 'ARRAY');
 
     return $self;
+}
+
+sub _to_query {
+    my $self  = shift;
+    my $query = shift;
+
+    return '' unless $query && reftype($query) eq 'HASH';
+
+    my $t_uri = URI->new('', 'http');
+    $t_uri->query_form(%$query);
+
+    return $t_uri->query;
 }
 
 1;
@@ -184,11 +233,118 @@ Mojo::CouchDB
 
       # You can also save many documents at a time
       my $books = $couch->save_many([{title => 'book', author => 'John'}, { title => 'foo', author => 'bar' }])->{docs};
+
+=head2 all_docs
+
+      $couch->all_docs({ limit => 10, skip => 5});
+
+    Retrieves a list of all of the documents in the database. This is packaged as a hashref with
+    C<offset>: the offset of the query, C<rows>: the documents in the page, and C<total_rows>: the number of rows returned in the dataset.
+
+    Optionally, can take a hashref of query parameters that correspond with the CouchDB L<view query specification|https://docs.couchdb.org/en/stable/api/ddoc/views.html#db-design-design-doc-view-view-name>.
+
+=head2 all_docs_p
+
+      $couch->all_docs_p({ limit => 10, skip => 5 });
+
+    See L<\"all_docs">, except returns the result in a L<Mojo::Promise>.
     
 =head2 create_db
 
       $couch->create_db
 
     Create the database, returns C<1> if succeeds or if it already exsits, else returns C<undef>.
+
+=head2 find
+
+      $couch->find($search_criteria);
+
+    Searches for records based on the search criteria specified. The search criteria 
+    hashref provided for searching must follow the CouchDB L<_find specification|https://docs.couchdb.org/en/stable/api/database/find.html#selector-syntax>.
+
+    Returns a hashref with two fields: C<docs> and C<execution_stats>. C<docs> contains an arrayref of found documents, while
+    C<execution_stats> contains a hashref of various statistics about the query you ran to find the documents.
+
+=head2 find_p
+
+      $couch->find_p($search_criteria);
+
+    See L<\"find">, except returns the result asynchronously in a L<Mojo::Promise>.
+
+=head2 index
+
+      $couch->index($idx);
+
+    Creates an index, where C<$idx> is a hashref following the CouchDB L<mango specification|https://docs.couchdb.org/en/stable/api/database/find.html#db-index>.
+    Returns the result of the index creation.
+
+=head2 index_p
+
+      $couch->index_p($idx);
+
+    See L<\"index">, except returns the result asynchronously in a L<Mojo::Promise>.
+
+=head2 new
+
+      my $url   = 'https://127.0.0.1:5984/my_database';
+      my $couch = Mojo::CouchDB->new($url, $username, $password);
+
+    Creates an instance of L<\"Mojo::CouchDB">. The URL specified must include the protocol either C<http> or C<https> as well
+    as the port your CouchDB instance is using, and the name of the database you want to manipulate.
+
+=head2 save
+
+      $couch->save($document);
+
+    Saves a document (hashref) to the database. If the C<_id> field is provided, it will update if it already exists.
+    If you provide both the C<_id> and C<_rev> field, that specific revision will be updated. Returns a hashref that corresponds
+    to the CouchDB C<POST /{db}> specification.
+
+=head2 save_p
+
+      $couch->save_p($document);
+
+    Does the same as L<\"save"> but instead returns the result asynchronously in a L<Mojo::Promise>.
+
+=head2 save_many
+
+      $couch->save_many($documents);
+
+    Saves an arrayref of documents (hashrefs) to the database. Each document follows the same rules as L<\"save">. Returns an
+    arrayref of the documents you saved with the C<_id> and C<_rev> fields filled.
+
+=head2 save_many_p
+
+      $couch->save_many_p($documents);
+
+    See L<\"save_many">, except returns the result asynchronously in a L<Mojo::Promise>.
+
+=head1 API
+
+=over 2
+
+=item * L<Mojo::CouchDB>
+
+=back
+
+=head1 AUTHOR
+
+Rawley Fowler, C<rawleyfowler@proton.me>.
+
+=head1 CREDITS
+
+=over 2
+
+=back
+
+=head1 LICENSE
+
+Copyright (C) 2023, Rawley Fowler and contributors.
+
+This program is free software, you can redistribute it and/or modify it under the terms of the Artistic License version 2.0.
+
+=head1 SEE ALSO
+
+L<https://mojolicious.org>.
 
 =cut
